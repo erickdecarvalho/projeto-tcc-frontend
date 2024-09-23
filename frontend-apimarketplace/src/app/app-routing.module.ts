340c62f0504c6d83bbb43c53ef057f59a98de649
erick7amorim <erickamorimcarv@gmail.com>
2024-09-23 11:12:02 -0300

    feat: create login page

--- frontend-apimarketplace/src/app/app-routing.module.ts
+++ frontend-apimarketplace/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './basic/home/home.component';
 import { RegisterComponent } from './basic/register/register.component';
+import { LoginComponent } from './basic/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'cadastro', component: RegisterComponent},
 ];
 
