006e1531924c48b6ffbfbf20f6acd65d5f094627
erick7amorim <erickamorimcarv@gmail.com>
2024-09-26 01:36:25 -0300

    feat: implement login functionality and auth service

--- frontend-apimarketplace/src/app/basic/components/login/login.component.ts
+++ frontend-apimarketplace/src/app/basic/components/login/login.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
+  validateForm!: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private authService : AuthService,
+    private router: Router) {
+  }
+
+  ngOnInit() {
+    this.validateForm = this.fb.group({
+      email: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+    })
+  }
+
+  submitForm() {
+    this.authService.login(this.validateForm.get(['email'])!.value, this.validateForm.get(['password'])!.value)
+    .subscribe(res => {
+      console.log(res);
+    }, error => {
+      alert("Dados incorretos!");
+    })
+  }
 }
